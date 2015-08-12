a3fbaaafaf0f10a125268be369c1231aece1aa7e
tangwz <tangwz.com@gmail.com>
2015-08-12 22:40:58 +0800

    ShellSort.

---
+++ algorithm/Sort/ShellSort.cpp
@@ -0,0 +1,31 @@
+/*
+ * Algorithm--ShellSort：https://en.wikipedia.org/wiki/Shellsort
+ * 
+ * Author:       Tangwz
+ * Data:         2015/8/12
+ * Blog:         tangwz.com
+ */
+
+/** 数组元素的类型 */
+typedef int elem_t;
+/*
+* @brief 一趟希尔插入排序.
+**
+和一趟直接插入排序相比，仅有一点不同，就是前后元素的间距是 gap 而不是 1
+*
+* @param[inout] a 待排序元素序列
+* @param[in] start 开始位置
+* @param[in] end 结束位置，最后一个元素后一个位置，即左闭右开区间
+* @param[in] gap 间隔
+* @return 无
+*/
+static void shell_insert(elem_t a[], const int start, const int end, const int gap) 
+{
+	elem_t tmp;
+	int i, j;
+
+	for(i = start + gap; i < end; ++i){
+		tmp = a[i];
+		for(j = i - gap; )
+	}
+}

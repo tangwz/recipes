1d13232745c19ca0c3a59d042d3c653bdc7963ae
tangwz <tangwz.com@gmail.com>
2015-08-18 23:13:52 +0800

    add test

---
+++ algorithm/test.cpp
@@ -0,0 +1,13 @@
+
+#include "Sort/BubbleSort.hpp"
+#include "Sort/BucketSort.hpp"
+#include "Sort/HeapSort.hpp"
+#include "Sort/InsertionSort.hpp"
+#include "Sort/MergeSort.hpp"
+#include "Sort/QuickSort.hpp"
+#include "Sort/RadixSort.hpp"
+#include "Sort/SelectionSort.hpp"
+#include "Sort/ShellSort.hpp"
+
+#include "Search/BinarySearch.hpp"
+#include "Search/HashTable.hpp"

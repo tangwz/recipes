49a4616d7dd241a5699720c971c3cb7f8a252b0f
Tang Weizhi <tangwz.com@gmail.com>
2016-09-13 20:43:38 +0800

    add comment.

--- tpc/InetAddress.cc
+++ tpc/InetAddress.cc
@@ -10,6 +10,7 @@ InetAddress::InetAddress(uint16_t port, bool loopbackOnly)
 {
     ::bzero(&saddr_, sizeof(saddr_));
     saddr_.sin_family = AF_INET;
+    // INADDR_LOOPBACK: can only receive 127.0.0.1
     saddr_.sin_addr.s_addr = htonl(loopbackOnly ? INADDR_LOOPBACK : INADDR_ANY);
     saddr_.sin_port = htons(port);
 }
